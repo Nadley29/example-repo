2593e540ed59cd2c3070480dc9248a9b7c628519
Andile <andile70@gmail.com>
2025-05-23 18:03:27 +0200

    Initial commit with hello_world.py file

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+# request user to input 
+name = input("Enter your name: ")
+# Display user name
+print(name)
+# request user to input age 
+age = input("Enter your age: ")
+# Display user age 
+print(age)
+# Display Hello world string
+print("Hello, World!")
